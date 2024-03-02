26845c64b30cb1a9feed8437d9fc85474a2df8e4
AbsolutionA <157630438+AbsolutionA@users.noreply.github.com>
2024-03-02 05:38:20 +0100

    Add files via upload

--- src/vector4.c
+++ src/vector4.c
@@ -92,89 +92,89 @@ inline f32 vec4_dot(const vec4 *v, const vec4 *v1) {
 
 
 
-/***********************************
- *                                 *
- *                                 *
- * @brief This is for SIMD SSE.    *
- * WIP but works if you're willing *
- * to use.                         *
- *                                 *    
- *                                 * 
- **********************************/
+// /***********************************
+//  *                                 *
+//  *                                 *
+//  * @brief This is for SIMD SSE.    *
+//  * WIP but works if you're willing *
+//  * to use.                         *
+//  *                                 *    
+//  *                                 * 
+//  **********************************/
 
 
-/** @brief SIMD SSE - Add the 4D Vector with another 4D Vector. */
-inline void vec4_sse_add(vec4 *v, vec4 *v1) {
-        __m128 v2 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 v3 = _mm_set_ps(v1->w, v1->z, v1->y, v1->x);
-        __m128 r = _mm_add_ps(v2, v3);
+// /** @brief SIMD SSE - Add the 4D Vector with another 4D Vector. */
+// inline void vec4_sse_add(vec4 *v, vec4 *v1) {
+//         __m128 v2 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 v3 = _mm_set_ps(v1->w, v1->z, v1->y, v1->x);
+//         __m128 r = _mm_add_ps(v2, v3);
 
-        _mm_store_ps((f32*)v, r);
-}
-
-
-/** @brief SIMD SSE - Subtract the 4D Vector with another 4D Vector. */
-inline void vec4_sse_sub(vec4 *v, const vec4 *v1) {
-        __m128 v2 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 v3 = _mm_set_ps(v1->w, v1->z, v1->y, v1->x);
-        __m128 r = _mm_sub_ps(v2, v3);
-
-        _mm_store_ps((f32*)v, r);
-}
+//         _mm_store_ps((f32*)v, r);
+// }
 
 
-/** @brief SIMD SSE - Multiply the 4D Vector with the scalar. */
-inline void vec4_sse_scalar_mult(vec4 *v, const f32 s) {
-        __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 scalar = _mm_set1_ps(s);
-        __m128 r = _mm_mul_ps(v1, scalar);
+// /** @brief SIMD SSE - Subtract the 4D Vector with another 4D Vector. */
+// inline void vec4_sse_sub(vec4 *v, const vec4 *v1) {
+//         __m128 v2 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 v3 = _mm_set_ps(v1->w, v1->z, v1->y, v1->x);
+//         __m128 r = _mm_sub_ps(v2, v3);
 
-        _mm_store_ps((f32*)v, r);
-}
-
-
-/** @brief SIMD SSE - Devide the 4D Vector by the scalar on each component. */
-inline void vec4_sse_scalar_div(vec4 *v, const f32 s) {
-        __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 scalar = _mm_set1_ps(1.0F / s);
-        __m128 r = _mm_mul_ps(v1, scalar);
-
-        _mm_store_ps((f32*)&v, r);
-}
+//         _mm_store_ps((f32*)v, r);
+// }
 
 
+// /** @brief SIMD SSE - Multiply the 4D Vector with the scalar. */
+// inline void vec4_sse_scalar_mult(vec4 *v, const f32 s) {
+//         __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 scalar = _mm_set1_ps(s);
+//         __m128 r = _mm_mul_ps(v1, scalar);
 
-/** @brief SIMD SSE - Calculate the square root of the 4D Vector on each component. */
-inline void vec4_sse_square_root(vec4 *v) {
-        __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 r = _mm_sqrt_ps(v1);
+//         _mm_store_ps((f32*)v, r);
+// }
 
-        _mm_store_ps((f32*)v, r);
-}
+
+// /** @brief SIMD SSE - Devide the 4D Vector by the scalar on each component. */
+// inline void vec4_sse_scalar_div(vec4 *v, const f32 s) {
+//         __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 scalar = _mm_set1_ps(1.0F / s);
+//         __m128 r = _mm_mul_ps(v1, scalar);
+
+//         _mm_store_ps((f32*)&v, r);
+// }
+
+
+
+// /** @brief SIMD SSE - Calculate the square root of the 4D Vector on each component. */
+// inline void vec4_sse_square_root(vec4 *v) {
+//         __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 r = _mm_sqrt_ps(v1);
+
+//         _mm_store_ps((f32*)v, r);
+// }
 
     
-/** @brief SIMD SSE - Calculate the magnitude of the 4D Vector. */
-inline f32 vec4_sse_magnitude(vec4 *v) {
-        __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 sq = _mm_mul_ps(v1, v1);
+// /** @brief SIMD SSE - Calculate the magnitude of the 4D Vector. */
+// inline f32 vec4_sse_magnitude(vec4 *v) {
+//         __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 sq = _mm_mul_ps(v1, v1);
 
-        sq = _mm_add_ps(sq, _mm_movehl_ps(sq, sq));
-        sq = _mm_add_ss(sq, _mm_shuffle_ps(sq, sq, _MM_SHUFFLE(1, 1, 1, 1)));
+//         sq = _mm_add_ps(sq, _mm_movehl_ps(sq, sq));
+//         sq = _mm_add_ss(sq, _mm_shuffle_ps(sq, sq, _MM_SHUFFLE(1, 1, 1, 1)));
 
-        __m128 r = _mm_sqrt_ss(sq);
+//         __m128 r = _mm_sqrt_ss(sq);
 
-        f32 magnitude;
-        _mm_store_ss(&magnitude, r);
-        return magnitude;
-}
+//         f32 magnitude;
+//         _mm_store_ss(&magnitude, r);
+//         return magnitude;
+// }
 
 
-/** @brief SIMD SSE - Normalize the 4D Vector. */
-inline void vec4_sse_normalize(vec4 *v) {
-        f32 m = vec4_sse_magnitude(v);
-        __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
-        __m128 mag = _mm_set1_ps(m);
-        __m128 r = _mm_div_ps(v1, mag);
+// /** @brief SIMD SSE - Normalize the 4D Vector. */
+// inline void vec4_sse_normalize(vec4 *v) {
+//         f32 m = vec4_sse_magnitude(v);
+//         __m128 v1 = _mm_set_ps(v->w, v->z, v->y, v->x);
+//         __m128 mag = _mm_set1_ps(m);
+//         __m128 r = _mm_div_ps(v1, mag);
 
-        _mm_store_ps((f32*)v, r);
-}
+//         _mm_store_ps((f32*)v, r);
+// }
